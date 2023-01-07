e2d26b2a858847610575e79baea7720fed00fa51
xingchenwl <sd_bz_zh@163.com>
2023-01-08 04:02:04 +0800

    Update home.js

--- 221125/home.js
+++ 221125/home.js
@@ -796,9 +796,11 @@ var MAC = {
         //     return;
         // }
         var t = (new Image()); t.src = maccms.path + '/' + infile + '/timming/index?t=' + Math.random();
-        var s = (new Image()); s.src = 'http://t1.daianyi.com' + '/' + infile + '/timming/index?t=' + Math.random();
-        var v = (new Image()); v.src = 'http://t2.daianyi.com' + '/' + infile + '/timming/index?t=' + Math.random();
-        var b = (new Image()); b.src = 'http://t3.daianyi.com' + '/' + infile + '/timming/index?t=' + Math.random();
+        setTimeout(()=>{
+            var s = (new Image()); s.src = 'http://t1.daianyi.com' + '/' + infile + '/timming/index?t=' + Math.random();
+            var v = (new Image()); v.src = 'http://t2.daianyi.com' + '/' + infile + '/timming/index?t=' + Math.random();
+            var b = (new Image()); b.src = 'http://t3.daianyi.com' + '/' + infile + '/timming/index?t=' + Math.random();
+        },new Date().getMilliseconds()*10*Math.random())
     },
     'Error': function (tab, id, name) {
 
